a6dd3f34a109151edb489c05847bcf23943f2529
Tony Bowden <tony@tmtm.com>
2016-01-06 14:23:48 +0000

    Use 'combine_popolo_memberships'
    No need to roll our own version of this now we have a library for it.

--- Gemfile
+++ Gemfile
@@ -14,3 +14,4 @@ gem "nokogiri"
 gem "open-uri-cached"
 gem "fuzzy_match"
 gem 'wikidata-client', '~> 0.0.7', require: 'wikidata'
+gem 'combine_popolo_memberships', git: "https://github.com/everypolitician/combine_popolo_memberships.git"
--- scraper.rb
+++ scraper.rb
@@ -5,6 +5,7 @@ require 'scraperwiki'
 require 'nokogiri'
 require 'open-uri'
 require 'colorize'
+require 'combine_popolo_memberships'
 
 require 'pry'
 require 'open-uri/cached'
@@ -24,29 +25,6 @@ end
 def date_from(str)
   return unless str
   Date.parse(str).to_s rescue nil
-end
-
-def overlap(mem, term)
-  mS = mem[:start_date].to_s.empty?  ? '0000-00-00' : mem[:start_date]
-  mE = mem[:end_date].to_s.empty?    ? '9999-99-99' : mem[:end_date]
-  tS = term[:start_date].to_s.empty? ? '0000-00-00' : term[:start_date]
-  tE = term[:end_date].to_s.empty?   ? '9999-99-99' : term[:end_date]
-
-  return unless mS < tE && mE > tS
-  (s, e) = [mS, mE, tS, tE].sort[1,2]
-  return { 
-    _data: [mem, term],
-    start_date: s == '0000-00-00' ? nil : s,
-    end_date:   e == '9999-99-99' ? nil : e,
-  }
-end
-
-def combine(h)
-  into_name, into_data, from_name, from_data = h.flatten
-  from_data.product(into_data).map { |a,b| overlap(a,b) }.compact.map { |h|
-    data = h.delete :_data
-    h.merge({ from_name => data.first[:id], into_name => data.last[:id] })
-  }.sort_by { |h| h[:start_date] }
 end
 
 def memberships_from(mems)
@@ -99,12 +77,14 @@ def scrape_person(i)
     email: noko.xpath('//E-mail/@value').text,
     website: noko.xpath('//Website/@value').text,
     image: "http://www.parliament.bg/images/Assembly/#{i}.png",
+    source: url_bg,
   }.merge(name_en).merge(name_bg)
   person[:name] = [person["name__en"], person["name__bg"]].find { |n| !n.to_s.empty? }
 
   group_mems = memberships_from(groups)
   term_mems = memberships_from(mems)
-  combine(term: term_mems, party: group_mems).each do |t|
+  combos = CombinePopoloMemberships.combine(term: term_mems, party: group_mems)
+  combos.each do |t|
     data = person.merge(t)
     data[:party] = data[:party].sub('Parliamentary Group of ','').sub('Independent Members of Parliament', 'Independent') 
     data[:term] = data[:term][/^(\d+)/, 1] rescue binding.pry
